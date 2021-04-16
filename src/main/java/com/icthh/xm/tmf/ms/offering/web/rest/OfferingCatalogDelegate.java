4baad71ef3ffdefeb320741600bb55cb6c5ce9b0
anton.mostipan <61111NewPassword>
2021-04-16 17:46:05 +0300

    Update swagger specification according to tmf spec https://github.com/tmforum-apis/TMF620_ProductCatalog/blob/master/TMF620-ProductCatalog-v4.0.0.swagger.json

--- src/main/java/com/icthh/xm/tmf/ms/offering/web/rest/OfferingCatalogDelegate.java
+++ src/main/java/com/icthh/xm/tmf/ms/offering/web/rest/OfferingCatalogDelegate.java
@@ -1,21 +1,19 @@
 package com.icthh.xm.tmf.ms.offering.web.rest;
 
+import static java.util.Arrays.asList;
+
 import com.icthh.xm.commons.lep.LogicExtensionPoint;
 import com.icthh.xm.commons.lep.spring.LepService;
 import com.icthh.xm.commons.permission.annotation.PrivilegeDescription;
-import com.icthh.xm.tmf.ms.offering.lep.keyresolver.ProfileChannelKeyResolver;
 import com.icthh.xm.tmf.ms.offering.lep.keyresolver.ProfileKeyResolver;
 import com.icthh.xm.tmf.ms.offering.web.api.CategoryApiDelegate;
 import com.icthh.xm.tmf.ms.offering.web.api.model.Category;
 import io.micrometer.core.annotation.Timed;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
-
-import static java.util.Arrays.asList;
 
 @Slf4j
 @Component
@@ -26,12 +24,8 @@ public class OfferingCatalogDelegate implements CategoryApiDelegate {
     @Override
     @PrivilegeDescription("Privilege to get a product offering category")
     @LogicExtensionPoint(value = "ListCategory", resolver = ProfileKeyResolver.class)
-    @PreAuthorize("hasPermission({'profile': @headerRequestExtractor.get('Profile')}, 'QUALIFICATION.PRODUCT.OFFERING.GET')")
-    public ResponseEntity<List<Category>> listCategory(String channel,
-                                                       String profile,
-                                                       String acceptLanguage,
-                                                       Integer offset,
-                                                       Integer limit) {
+    @PreAuthorize("hasPermission({'profile': @headerRequestExtractor.get('Profile')}, 'OFFERING.CATEGORY.LIST')")
+    public ResponseEntity<List<Category>> listCategory(String fields, Integer offset, Integer limit) {
         getRequest().ifPresent(request -> log.info("Native request {}", getRequest().get().getNativeRequest()));
         return ResponseEntity.ok(asList(new Category()));
     }
