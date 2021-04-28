d8f93254a5d0fe2b7c867da160833723e85748fc
anton.mostipan <61111NewPassword>
2021-04-28 23:32:38 +0300

    to lowe case

--- src/main/java/com/icthh/xm/tmf/ms/offering/lep/keyresolver/ProfileChannelKeyResolver.java
+++ src/main/java/com/icthh/xm/tmf/ms/offering/lep/keyresolver/ProfileChannelKeyResolver.java
@@ -4,8 +4,10 @@ import com.icthh.xm.commons.lep.AppendLepKeyResolver;
 import com.icthh.xm.lep.api.LepManagerService;
 import com.icthh.xm.lep.api.LepMethod;
 import com.icthh.xm.lep.api.commons.SeparatorSegmentedLepKey;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class ProfileChannelKeyResolver extends AppendLepKeyResolver {
 
@@ -13,6 +15,9 @@ public class ProfileChannelKeyResolver extends AppendLepKeyResolver {
     protected String[] getAppendSegments(SeparatorSegmentedLepKey baseKey,
                                          LepMethod method,
                                          LepManagerService managerService) {
+        log.info(" params {} ", method.getMethodSignature().getName() );
+        log.info(" params {} ", method.getMethodArgValues());
+        log.info(" params {}  ", method.getMethodSignature().getMethod().getName());
         String profile = getRequiredParam(method, "profile", String.class);
         String channel = getRequiredParam(method, "channel", String.class);
         return new String[]{translateToLepConvention(profile), translateToLepConvention(channel)};
