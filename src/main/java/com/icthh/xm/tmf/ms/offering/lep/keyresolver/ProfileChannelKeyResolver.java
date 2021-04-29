fbb8d6ce047dfe9341d19a2cd0e864f4d2bc9f46
yekalinichenko <pkev777@gmail.com>
2021-04-29 18:43:21 +0300

    Changed lep resolver to profile, fixed permissions

--- src/main/java/com/icthh/xm/tmf/ms/offering/lep/keyresolver/ProfileChannelKeyResolver.java
+++ src/main/java/com/icthh/xm/tmf/ms/offering/lep/keyresolver/ProfileChannelKeyResolver.java
@@ -4,17 +4,22 @@ import com.icthh.xm.commons.lep.AppendLepKeyResolver;
 import com.icthh.xm.lep.api.LepManagerService;
 import com.icthh.xm.lep.api.LepMethod;
 import com.icthh.xm.lep.api.commons.SeparatorSegmentedLepKey;
+import com.icthh.xm.tmf.ms.offering.web.rest.util.HeaderRequestExtractor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class ProfileChannelKeyResolver extends AppendLepKeyResolver {
+
+    private final HeaderRequestExtractor headerRequestExtractor;
 
     @Override
     protected String[] getAppendSegments(SeparatorSegmentedLepKey baseKey,
                                          LepMethod method,
                                          LepManagerService managerService) {
-        String profile = getRequiredParam(method, "Profile", String.class);
-        String channel = getRequiredParam(method, "Channel", String.class);
+        String profile = headerRequestExtractor.get("Profile");
+        String channel = headerRequestExtractor.get("Channel");
         return new String[]{translateToLepConvention(profile), translateToLepConvention(channel)};
     }
 }
